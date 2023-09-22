0ee6c08eba9bc498b39b7f4e0a40e9d96bd2e8b0
wanjun3211 <118150417+wanjun3211@users.noreply.github.com>
2023-09-22 19:41:11 +1200

    Add files via upload

---
+++ smarthome693.sql
@@ -0,0 +1,14 @@
+drop schema if exists `smart_home693`;
+CREATE DATABASE `smart_home693`;
+USE `smart_home693`;
+
+
+CREATE TABLE equipment (
+  equ_id VARCHAR(100) PRIMARY KEY,
+  brand VARCHAR(30),
+  equ_descr VARCHAR(30)
+  add_flag 
+);
+
+
+
